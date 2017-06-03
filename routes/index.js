ab0db588aca36ce0d1840312b2b5661749efde95
santipantin94 <s.pantin94@gmail.com>
2017-06-03 22:16:22 +0200

    Practica 5.2 - Juego Aleatorio

--- routes/index.js
+++ routes/index.js
@@ -36,7 +36,7 @@ router.delete('/quizzes/:quizId(\\d+)',    quizController.destroy);
 router.get('/quizzes/:quizId(\\d+)/play',  quizController.play);
 router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
 
-router.get('/quizzes/randomplay', quizController.random);
+router.get('/quizzes/randomplay', quizController.randomPlay);
 router.get('/quizzes/randomcheck/:quizId(\\d+)', quizController.randomCheck)
 
 
