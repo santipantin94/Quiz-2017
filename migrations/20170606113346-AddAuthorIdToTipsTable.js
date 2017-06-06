159b286346c6d237c334478298514d77b2eeb1d4
santipantin94 <s.pantin94@gmail.com>
2017-06-06 13:42:11 +0200

    Practica 5.3 - Pista con Autor

--- migrations/20170606113346-AddAuthorIdToTipsTable.js
+++ migrations/20170606113346-AddAuthorIdToTipsTable.js
@@ -1,15 +1,15 @@
-+'use strict';
- +
- +module.exports = {
- +    up: function (queryInterface, Sequelize) {
- +        return queryInterface.addColumn(
- +            'Tips',
- +            'AuthorId',
- +            {type: Sequelize.INTEGER}
- +        );
- +    },
- +
- +    down: function (queryInterface, Sequelize) {
- +        return queryInterface.removeColumn('Quizzes', 'AuthorId');
- +    }
- +};
+'use strict';
+ 
+ module.exports = {
+     up: function (queryInterface, Sequelize) {
+         return queryInterface.addColumn(
+             'Tips',
+             'AuthorId',
+             {type: Sequelize.INTEGER}
+         );
+     },
+ 
+     down: function (queryInterface, Sequelize) {
+         return queryInterface.removeColumn('Quizzes', 'AuthorId');
+     }
+ };
