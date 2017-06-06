7530830703210461d20572002690396845c3b8c1
santipantin94 <s.pantin94@gmail.com>
2017-06-06 13:41:10 +0200

    Practica 5.3 - Pista con Autor

--- migrations/20170606113346-AddAuthorIdToTipsTable.js
+++ migrations/20170606113346-AddAuthorIdToTipsTable.js
@@ -1,23 +1,15 @@
-'use strict';
-
-module.exports = {
-  up: function (queryInterface, Sequelize) {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
-  },
-
-  down: function (queryInterface, Sequelize) {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
-  }
-};
++'use strict';
+ +
+ +module.exports = {
+ +    up: function (queryInterface, Sequelize) {
+ +        return queryInterface.addColumn(
+ +            'Tips',
+ +            'AuthorId',
+ +            {type: Sequelize.INTEGER}
+ +        );
+ +    },
+ +
+ +    down: function (queryInterface, Sequelize) {
+ +        return queryInterface.removeColumn('Quizzes', 'AuthorId');
+ +    }
+ +};
